de0e8b2858bbdb56a192877de9fe234892227199
Ubuntu <ubuntu@ip-172-31-11-74.ec2.internal>
2026-08-24 17:49:07 +0000

    Configure Terraform S3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "techpathway-terraform-state-285851439149"
+    key    = "techpathway/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
